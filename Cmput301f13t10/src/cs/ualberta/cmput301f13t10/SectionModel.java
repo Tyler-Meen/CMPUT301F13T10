f82665b45d99ba7444765286ef3bde7c1fa6345b
bacowan <bacowan@ualberta.ca>
2013-10-31 12:09:21 -0700

    missed a couple of things

--- Cmput301f13t10/src/cs/ualberta/cmput301f13t10/SectionModel.java
+++ Cmput301f13t10/src/cs/ualberta/cmput301f13t10/SectionModel.java
@@ -1,5 +1,18 @@
 package cs.ualberta.cmput301f13t10;
 
 public class SectionModel {
-
+	
+	private boolean mIsStart;
+	
+	public SectionModel() {
+		setStart(false);
+	}
+	
+	public SectionModel(boolean isStart){
+		setStart(isStart);
+	}
+	
+	private void setStart(boolean isStart) {
+		mIsStart = isStart;
+	}
 }
--- Cmput301f13t10/src/cs/ualberta/test/SectionModelTest.java
+++ Cmput301f13t10/src/cs/ualberta/test/SectionModelTest.java
@@ -2,13 +2,35 @@ package cs.ualberta.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class SectionModelTest {
+import cs.ualberta.cmput301f13t10.AdventurePresenter;
 
+public class SectionModelTest {
+	
+	public SectionModelTest() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddMedia() {
+	}
+	
+	@Test
+	public void testRemoveMedia() {
+	}
+	
+	@Test
+	public void testMoveMedia() {
 	}
 
 }
