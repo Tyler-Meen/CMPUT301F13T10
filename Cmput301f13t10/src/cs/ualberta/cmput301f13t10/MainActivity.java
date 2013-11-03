8693418c86b357f2c1576b226f2bca776d49ee05
bsoetaer <bsoetaer@ualberta.ca>
2013-11-03 00:48:23 -0600

    Created the main menu.
    Buttons that should go to other views that are not yet created are
    commented out. Uncomment them when the classes are created.

--- Cmput301f13t10/src/cs/ualberta/cmput301f13t10/MainActivity.java
+++ Cmput301f13t10/src/cs/ualberta/cmput301f13t10/MainActivity.java
@@ -1,11 +1,22 @@
 package cs.ualberta.cmput301f13t10;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
+	public void editAdventures(View view){
+//		Intent intent = new Intent(this, AdventureEditView.class);
+//		startActivity(intent);
+	}
+	
+	public void help(View view){
+//		Intent intent = new Intent(this, HelpView.class);
+//		startActivity(intent);
+	}
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
@@ -20,6 +31,16 @@ public class MainActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate( R.menu.main, menu );
 		return true;
+	}
+	
+	public void readAdventures(View view){
+		Intent intent = new Intent(this, AdventureReadView.class);
+		startActivity(intent);
+	}
+	
+	public void settings(View view){
+//		Intent intent = new Intent(this, SettingsView.class);
+//		startActivity(intent);
 	}
 
 }
