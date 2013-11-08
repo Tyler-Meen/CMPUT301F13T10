6a698192576241324efdebee4343e6103161e753
bacowan <bacowan@ualberta.ca>
2013-11-07 21:40:22 -0800

    comments!

--- Cmput301f13t10/src/cmput301f13t10/AppConstants.java
+++ Cmput301f13t10/src/cmput301f13t10/AppConstants.java
@@ -10,13 +10,43 @@ package cmput301f13t10;
  */
 public class AppConstants
 {
+	/**
+	 * Tag. Indicates that this is a list of possible choices in a bundle.
+	 */
 	public static final String CHOICES_BUNDLE = "choices_bundle";
+	
+	/**
+	 * Default title of the start section.
+	 */
 	public static final String START = "Start";
+	
+	/**
+	 * Tag. Indicates that this is an adventure_read_view.
+	 */
 	public static final String ADVENTURE_READ_VIEW = "adventure_read_view";
+	
+	/**
+	 * Tag. Indicates that this is an adventure_edit_view.
+	 */
 	public static final String ADVENTURE_EDIT_VIEW = "adventure_edit_view";
+	
+	/**
+	 * Tag. Indicates that this is the current adventure.
+	 */
 	public static final String CURRENT_ADVENTURE = "current_adventure";
-	public static final float PAGE_TURN_VELOCITY = 1;
+	
+	/**
+	 * Tag. Indicates that this is an adventure id.
+	 */
 	public static final String ADVENTURE_ID = "adventure_id";
+	
+	/**
+	 * Tag. Indicates that this is an section id.
+	 */
 	public static final String SECTION_ID = "section_id";
+	
+	/**
+	 * Tag. Indicates that this is a section title.
+	 */
 	public static final String SECTION_TITLE = "section_title";
 }
