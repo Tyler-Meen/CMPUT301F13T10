0c43c052e4a7e201c183cfcf0da65d7ef590c6a9
bacowan <bacowan@ualberta.ca>
2013-11-07 21:41:40 -0800

    comments!

--- Cmput301f13t10/src/cmput301f13t10/SectionView.java
+++ Cmput301f13t10/src/cmput301f13t10/SectionView.java
@@ -2,8 +2,20 @@ package cmput301f13t10;
 
 import android.content.Context;
 
+/**
+ * Interface for section views. Can update the view, and get the context of the view.
+ * @author Steven Gerdes
+ *
+ */
 public interface SectionView
 {
+	/**
+	 * Update the section view with new data.
+	 */
 	public void updateSectionView();
+	/**
+	 * Get the context of the section view
+	 * @return The context of the section view.
+	 */
 	public Context getContext();
 }
