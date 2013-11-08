084e03afba06fca829eee20a284cd5706f29186e
bacowan <bacowan@ualberta.ca>
2013-11-07 21:42:51 -0800

    comments!

--- Cmput301f13t10/src/cmput301f13t10/SectionNotFoundException.java
+++ Cmput301f13t10/src/cmput301f13t10/SectionNotFoundException.java
@@ -1,7 +1,15 @@
 package cmput301f13t10;
 
+/**
+ * Indicates that a requested section does not exist
+ * @author Steven Gerdes
+ *
+ */
 public class SectionNotFoundException extends Exception
 {
+	/**
+	 * Constructor.
+	 */
 	SectionNotFoundException()
 	{
 		super();
